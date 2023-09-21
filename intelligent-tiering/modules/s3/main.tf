e7dc043fa8f836d199327a782200f11bc72d0c3b
Taehyun Kim <k1m743hyun@icloud.com>
2023-09-21 15:21:08 +0900

    Update intelligent-tiering

--- intelligent-tiering/modules/s3/main.tf
+++ intelligent-tiering/modules/s3/main.tf
@@ -1,7 +1,20 @@
 resource "aws_s3_bucket" "this" {
   bucket = var.bucket
+
+  force_destroy = true
 }
 
 resource "aws_s3_bucket_intelligent_tiering_configuration" "this" {
   bucket = aws_s3_bucket.this.id
+  name = var.bucket
+
+  tiering {
+    access_tier = "ARCHIVE_ACCESS"
+    days        = 30
+  }
+
+  tiering {
+    access_tier = "DEEP_ARCHIVE_ACCESS"
+    days        = 60
+  }
 }
