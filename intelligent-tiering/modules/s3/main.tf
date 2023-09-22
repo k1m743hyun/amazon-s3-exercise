036f3184f263732ddfcf4ff64efaaf0f3402f159
Taehyun Kim <k1m743hyun@icloud.com>
2023-09-22 13:58:48 +0900

    Apply to Archive Instant Access tier

--- intelligent-tiering/modules/s3/main.tf
+++ intelligent-tiering/modules/s3/main.tf
@@ -4,17 +4,29 @@ resource "aws_s3_bucket" "this" {
   force_destroy = true
 }
 
-resource "aws_s3_bucket_intelligent_tiering_configuration" "this" {
+#resource "aws_s3_bucket_intelligent_tiering_configuration" "this" {
+#  bucket = aws_s3_bucket.this.id
+#  name = var.bucket
+
+#  tiering {
+#    access_tier = "ARCHIVE_ACCESS"
+#    days        = 90
+#  }
+
+  #tiering {
+  #  access_tier = "DEEP_ARCHIVE_ACCESS"
+  #  days        = 180
+  #}
+#}
+
+resource "aws_s3_bucket_lifecycle_configuration" "this" {
   bucket = aws_s3_bucket.this.id
-  name = var.bucket
 
-  tiering {
-    access_tier = "ARCHIVE_ACCESS"
-    days        = 90
-  }
-
-  tiering {
-    access_tier = "DEEP_ARCHIVE_ACCESS"
-    days        = 180
+  rule {
+    id     = var.bucket
+    status = "Enabled"
+    transition {
+      storage_class = "INTELLIGENT_TIERING"
+    }
   }
 }
